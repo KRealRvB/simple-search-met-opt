26cc83fe868f3b0b38a607fe199d4a087a409b07
k_real <rovnovbeat@gmail.com>
2024-03-13 13:40:56 +0300

    version 1.0

--- simple_search.py
+++ simple_search.py
@@ -1,20 +1,20 @@
 import math
 
-start_interval = 0
-end_interval = 1
-x = start_interval
-dictionary_of_solution = {}
-epsilon = 0.2
-count_of_step = (end_interval - start_interval) // epsilon
+# start_interval = 0
+# end_interval = 1
+# x = start_interval
+# dictionary_of_solution = {}
+# epsilon = 0.2
+# count_of_step = (end_interval - start_interval) // epsilon
 
 
-def function_of_x(x):
-    return x**3 - math.sin(x)
+# def function_of_x(x):
+#     return x**3 - math.sin(x)
 
-for i in range(0, int(count_of_step) + 1):
-    function_of_x(x)
-    x += epsilon
-    dictionary_of_solution.fromkeys(x[function_of_x(x)])
+# for i in range(0, int(count_of_step) + 1):
+#     function_of_x(x)
+#     x += epsilon
+#     dictionary_of_solution.fromkeys(x[function_of_x(x)])
 
 
 # while x <= end_interval:
@@ -23,4 +23,37 @@ for i in range(0, int(count_of_step) + 1):
 #         x += epsilon
 #     list_of_solution.append(function_of_x(x))
 
-print(min(list_of_solution), x)
+
+
+
+
+
+
+start_interval = 0
+end_interval = 1
+x = start_interval
+dictionary_of_solution = {}
+epsilon = 0.2
+count_of_step = (end_interval - start_interval) / epsilon
+
+
+def function_of_x(x):
+    return x**3 - math.sin(x)
+
+for i in range(int(count_of_step + 1)):
+    dictionary_of_solution[x] = function_of_x(x)
+    x += epsilon
+
+print(dictionary_of_solution)
+
+list_of_func = []
+
+for values in dictionary_of_solution:
+    list_of_func.append(dictionary_of_solution[values])
+
+print(min(list_of_func))
+
+
+# print(f' минимальное значение функция f({dictionary_of_solution[]})min(list_of_func)')
+
+# надо писать функцию поиска ключа по значению. Да и вообще все это в функции запихнуть.
